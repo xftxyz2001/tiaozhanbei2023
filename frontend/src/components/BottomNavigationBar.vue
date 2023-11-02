57b4e56c4da211fa4b050a9c585b506b28328c8d
通晓宇宙 <2581011320@qq.com>
2023-11-02 11:42:39 +0800

    vue vant test

---
+++ frontend/src/components/BottomNavigationBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const active = ref(0)
+</script>
+
+<template>
+  <van-tabbar v-model="active">
+    <van-tabbar-item to="/genealogy" class="iconfont icon-jiazuzupu-">族谱</van-tabbar-item>
+    <van-tabbar-item to="/mailbox" class="iconfont icon-xinxiang-xiankuang">信箱</van-tabbar-item>
+    <van-tabbar-item to="/market" class="iconfont icon-gouwuche">集市</van-tabbar-item>
+    <van-tabbar-item to="/my" class="iconfont icon-wodewo">我的</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<style scoped></style>
